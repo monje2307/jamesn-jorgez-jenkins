a4f5d4528ed8fee9dc0b634eb8768cf85865aca6
james naranjo <monje2307@gmail.com>
2026-05-24 23:40:05 -0500

    prueba3

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,7 @@
-from enum import Enum
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+ from enum import Enum
 from tabulate import tabulate
 
 class EstadoPedido(Enum):
